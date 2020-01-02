2fe71ab580b7fa06fee7a032131f2ca0fd0b185f
DKerstma93 <denniskerstma@hotmail.com>
2020-01-02 18:33:47 +0100

    Stores Usernames in Database

--- V_Get_Statistics.py
+++ V_Get_Statistics.py
@@ -1,33 +1,19 @@
-from V_Get_Summoners import match_id_list
-from V_Get_Summoners import api
-import mysql.connector
 import time
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="Password123",
-  database="vilemawdb"
-
-)
-
-mycursor = mydb.cursor()
-
-def get_match_stats():
-  for x in match_id_list:
-    index = 0
-    match_stats = api.get_matchstats(x)
-    time.sleep(1.3)
-    while index < 10:
-      sql = "INSERT INTO summonerstats (win, championId, item0, item1, item2, item3, item4, item5) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)"
-      val = ((match_stats['participants'][index]['stats']['win']),(match_stats['participants'][index]['championId']),(match_stats['participants'][index]['stats']['item0']),(match_stats['participants'][index]['stats']['item1']),(match_stats['participants'][index]['stats']['item2']),(match_stats['participants'][index]['stats']['item3']),(match_stats['participants'][index]['stats']['item4']),(match_stats['participants'][index]['stats']['item5']))
-      mycursor.execute(sql, val)
-      mydb.commit()
-      index += 1
-  # while index < 10:
-  #   print(match_stats['participants'][index]['stats']['item0'])
-  #   print(match_stats['participants'][index]['championId'])
-  #   index+=1
+# mycursor = mydb.cursor()
+#
+# def get_match_stats():
+#   for x in match_id_list:
+#     index = 0
+#     match_stats = api.get_matchstats(x)
+#     time.sleep(1.3)
+#     while index < 10:
+#       sql = "INSERT INTO summonerstats (win, championId, item0, item1, item2, item3, item4, item5) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)"
+#       val = ((match_stats['participants'][index]['stats']['win']),(match_stats['participants'][index]['championId']),(match_stats['participants'][index]['stats']['item0']),(match_stats['participants'][index]['stats']['item1']),(match_stats['participants'][index]['stats']['item2']),(match_stats['participants'][index]['stats']['item3']),(match_stats['participants'][index]['stats']['item4']),(match_stats['participants'][index]['stats']['item5']))
+#       mycursor.execute(sql, val)
+#       mydb.commit()
+#       index += 1
+#
 
 
 
--- V_Get_Summoners.py
+++ V_Get_Summoners.py
@@ -1,9 +1,11 @@
+import itertools
 from V_API import V_API
 import time
+from V_Database import mydb
 
 
-api = V_API('RGAPI-8ae61627-2df0-46a4-a39e-ae5e9bf8b2f6') #Insert API key
-
+api = V_API('RGAPI-bc5f9e3d-a63c-4624-9db1-d533f33f417c') #Insert API key
+cursor = mydb.cursor()
 
 summoner_list = []
 account_id_list = []
@@ -16,34 +18,40 @@ def save_summoners():
      for j in tier_list:
          for i in division_list:
             request_summoners = api.get_summoners('RANKED_SOLO_5x5',j,i)
-            time.sleep(1.3)
+            time.sleep(1)
             for x in request_summoners:
-                summoner_list.append(x['summonerName'])
+                sql = "INSERT INTO summonernames (summonername, division) VALUES (%s,%s)"
+                val = (x['summonerName'],j + ' ' + i)
+                cursor.execute(sql, val)
+     mydb.commit()
 
 #Use summoner names to get encrypted summoner ids
 def save_saccount_id():
-      for x in summoner_list:
+    cursor.execute("select summonername from summonernames")
+    summoner_list = list(itertools.chain.from_iterable(cursor))
+    for x in summoner_list:
         try:
             request_summonerid = api.get_summoner_by_name(x)
-            time.sleep(1.3)
+            time.sleep(1)
             account_id_list.append(request_summonerid['accountId'])
         except:
             continue
 
 
-#Use account ids to request matchids
-def save_match_id():
-    for x in account_id_list:
-        try:
-            request_matchids = api.get_match_history(x)
-            time.sleep(1.3)
-            index = 0
-            for y in request_matchids['matches']:
-                if y not in match_id_list:
-                    match_id_list.append(request_matchids['matches'][index]['gameId'])
-                    index += 1
-        except:
-            continue
-
+# #Use account ids to request matchids
+# def save_match_id():
+#     for x in account_id_list:
+#         request_matchids = api.get_match_history(x)
+#         time.sleep(1)
+#         index = 0
+#         try:
+#             for y in request_matchids['matches']:
+#                 if y not in match_id_list:
+#                     match_id_list.append(request_matchids['matches'][index]['gameId'])
+#                     index += 1
+#         except:
+#             index +=1
+#             continue
+#
 
 
